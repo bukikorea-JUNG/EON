d493d28fb7eee51d1e733cc75077b524fd7ef40c
bukikorea-JUNG <bukikorea@gmail.com>
2026-08-31 02:57:53 +0900

    Delete japangolf/supabase_patch_linked_listing.sql

--- japangolf/supabase_patch_linked_listing.sql
+++
@@ -1,4 +0,0 @@
--- 기존 DB에 패치
- alter table inquiries add column if not exists linked_listing text;
- alter table inquiries add column if not exists assigned_to text;
- alter table inquiries add column if not exists follow_up_date date;
